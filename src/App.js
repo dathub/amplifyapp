169a776101fa7efcd5741d4515c918ba42c64fc6
Dumindu Thilakaratne <dumindu.thilakaratne@lseg.com>
2021-06-25 18:55:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React. Rosa Achchi!!!!
         </a>
       </header>
     </div>
